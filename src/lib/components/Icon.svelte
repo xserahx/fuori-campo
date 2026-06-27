502130b39fd1c9ab9b422291d2b62fdc1bba2aca
Matilde Curino <matilde.curino@mail.polimi.it>
2026-06-27 09:27:58 +0200

    aggiunta componente per phosphoricons

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,30 @@
+<script>
+    const { name, variant = "regular"} = $props();
+
+    let pack = $derived.by(() => {
+        switch (variant) {
+            case "regular": 
+                return "ph";
+            case"fill":
+                return "ph-fill";
+            default: 
+                return "ph";
+        }
+    });
+</script>
+
+<svelte:head>
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css"
+    />
+
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css"
+    />
+</svelte:head>
+  
+<i class="{pack} ph-{name}"></i> 
