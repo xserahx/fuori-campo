d7d6a6d5005ab7e96f4e15241845d6fd0f429f94
xserahx <xxladantessaxx@gmail.com>
2026-06-29 18:14:43 +0200

    change index name

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,9 @@ const config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
+		files: {
+			template: 'src/index.html'
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
