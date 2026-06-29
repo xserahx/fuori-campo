84d1f1b3d2685736c6889b698ef3b99679effa2a
xserahx <xxladantessaxx@gmail.com>
2026-06-29 18:33:56 +0200

    changes for deploy

--- svelte.config.js
+++ svelte.config.js
@@ -16,15 +16,15 @@ const config = {
 		files: {
 			template: 'index.html'
 		},
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter({
-				pages: 'build',
-				assets: 'build',
-				fallback: 'index.html',
-				precompress: false,
-				strict: false
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/fuori-campo'
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: false
 		}),
 		prerender: {
 			handleMissingId: 'warn'
